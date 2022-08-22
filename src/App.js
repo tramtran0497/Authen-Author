9e6a0b72fc0e43677f78d3791fc6030175f0fa12
tramtran0497 <tramtran0497@gmail.com>
2022-08-22 21:47:36 +0300

    Adding react routers dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import { LogIn } from './Pages/LogIn.tsx';
-import Register from './Pages/Register.tsx';
+import {Register} from './Pages/Register.tsx';
+import { Routes, Route} from 'react-router-dom';
+import { Home } from './Pages/Home.tsx';
 
 function App() {
 
   return (
     <div className="App">
-    {/*<Register/> */}
-      <LogIn/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<LogIn />} />
+     
+    </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface HomeProps {}
+
+export const Home:React.FC<HomeProps> = () => {
+  return (
+    <div>Home</div>
+  )
+}
